0ed9b93211c101757a1cbc34ff2fc691cd2dfb3c
XNattapongX <hinokamikun5@hotmail.com>
2023-04-18 14:25:55 +0700

    แก้ไข

---
+++ src/pages/roomdetail.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="flex-row items-center justify-center pt-4 h-140 bg-gray-200 rounded-lg">
+        <div class="w-full max-w-screen-xl px-4 mx-auto lg:px-12">
+            <!-- start coding -->
+            <br>
+            <br>
+            <div
+                class="flex-row items-center justify-start  text-xl p-4 space-y-1 sm:flex sm:space-y-0 sm:space-x-4 text-white">
+                <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> สถานะห้องพัก </button>
+            </div>
+            <div
+                class="flex-row items-center justify-start bg-gray-400 text-xl p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4 text-white rounded-lg">
+                <h5 class="mr-3 font-semibold dark:text-white">หมายเลขห้อง : XXX</h5>
+            </div>
+            
+            <div
+                class="flex-row items-center justify-center text-xl p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4 text-white rounded-lg">
+                <a href="roomadduser">
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">เพิ่มผู้เช่า</button>
+                </a>
+                <a href="roombooking">
+                <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">จองห้องพัก</button>
+                </a>
+            </div>
+        <!-- end coding -->
+    </div>
+</section></template>
