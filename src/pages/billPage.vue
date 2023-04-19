8d8ee71b024c480395ba4a5493b32ed8e9e59bdf
Apiwan <gamebp02@hotmail.com>
2023-04-19 11:12:21 +0700

    แก้ไขคำหน้าบิล

--- src/pages/billPage.vue
+++ src/pages/billPage.vue
@@ -55,7 +55,7 @@
                               <tr>
                                   <th scope="col" class="px-4 py-3">สร้างใบแจ้งหนี้</th>
                                   <td class="px-4 py-3 flex items-center justify-end">
-                                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ดูใบแข้งหนี้</button>
+                                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ดูใบแจ้งหนี้</button>
                                     <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">ออกบิลทั้งหมด</button>
                                   </td>
                               </tr>
